85f28dce35938d363c242263345242d44773a214
nana-dl92 <kinhtevimoabc@gmail.com>
2025-06-13 14:34:33 +0700

    Fix: add base route for Render homepage

--- server/server.js
+++ server/server.js
@@ -15,6 +15,10 @@ app.use('/api/messages', require('./routes/messages.routes'));
 app.use('/api', require('./routes/auth.routes')); 
 app.use('/api', require('./routes/vocabulary.routes'));
 
+app.get('/', (req, res) => {
+  res.send('API Server is running.');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
